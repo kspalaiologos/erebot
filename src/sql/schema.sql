ea1ff472be22c9afac87b556862834743a3ef636
Kamila Szewczyk <kspalaiologos@gmail.com>
2025-04-11 11:14:15 +0200

    update db schema for messages

--- src/sql/schema.sql
+++ src/sql/schema.sql
@@ -8,15 +8,6 @@ CREATE TABLE IF NOT EXISTS "games" (
 	PRIMARY KEY("id" AUTOINCREMENT)
 );
 CREATE TABLE sqlite_sequence(name,seq);
-CREATE TABLE IF NOT EXISTS "tasks" (
-	"id"	INTEGER NOT NULL,
-	"game_id"	INTEGER NOT NULL,
-	"description"	TEXT NOT NULL,
-	"points"	INTEGER NOT NULL,
-	"test_program"	TEXT NOT NULL,
-	"score_program"	TEXT NOT NULL,
-	PRIMARY KEY("id" AUTOINCREMENT)
-);
 CREATE TABLE IF NOT EXISTS "solutions" (
 	"id"	INTEGER NOT NULL,
 	"submitter_id"	TEXT NOT NULL,
@@ -33,4 +24,29 @@ CREATE TABLE IF NOT EXISTS "validation_queue" (
 	"task_id"	INTEGER NOT NULL,
 	"score"	INTEGER NOT NULL,
 	PRIMARY KEY("id" AUTOINCREMENT)
-)
+);
+CREATE TABLE IF NOT EXISTS "tasks" (
+	"id"	INTEGER NOT NULL,
+	"game_id"	INTEGER NOT NULL,
+	"description"	TEXT NOT NULL,
+	"points"	INTEGER NOT NULL,
+	"score_program"	TEXT NOT NULL,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+CREATE TABLE IF NOT EXISTS "messages" (
+	"id"	INTEGER NOT NULL,
+	"recipient_id"	TEXT NOT NULL,
+	"message"	TEXT NOT NULL,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+CREATE INDEX "solutions_main_idx" ON "solutions" (
+	"id",
+	"submitter_id",
+	"task_id",
+	"score"
+);
+CREATE INDEX "tasks_main_idx" ON "tasks" (
+	"id",
+	"game_id",
+	"points"
+);
